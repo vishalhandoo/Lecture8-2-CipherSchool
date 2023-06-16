25b6a83ec0f53c80b9fffe594e0386fe368f88aa
vishalhandoo <81742803+vishalhandoo@users.noreply.github.com>
2023-06-16 22:07:39 +0530

    Add files via upload

---
+++ Lecture8part1.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printstuff(){
+    cout<<"Writting My First Function"<<endl;
+}
+
+int main(){
+    // int password;            while loop
+
+    // cout<<"Enter the password: "<<endl;
+    // cin>>password;
+
+    // while(password<99999){
+    //     cout<<"The password does not meet the required condition,Please enter the password again"<<endl;
+    //     cin>>password;
+    // }
+
+    // cout<<"The user has now entered the required password"<<endl;
+
+    printstuff();
+
+    return 0;
+}
